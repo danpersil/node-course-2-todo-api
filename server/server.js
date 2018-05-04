354ce6f7023602327c0e969d225d19cc0f63af4b
Daniel Pereira da Silva <AVA092@einstein.br>
2018-05-03 22:03:14 -0300

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ app.get('/todos/:id', (req, res) => {
 
   Todo.findById(id).then((todo) => {
     if(!todo){
-      res.status(404).send();
+      return res.status(404).send();
     }
     res.status(200).send({todo});
 
@@ -55,12 +55,12 @@ app.delete('/todos/:id', (req, res) => {
   var id = req.params.id;
 
   if(!ObjectID.isValid(id)){
-    res.status(404).send();
+    return res.status(404).send();
   }
 
   Todo.findByIdAndRemove(id).then((todo) => {
     if(todo){
-      res.send({todo});
+      return res.send({todo});
     }
     res.status(404).send();
   });
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -11,7 +11,7 @@ const todos = [{
 }, {
   _id: new ObjectID(),
   text: 'Second test to do'
-}]
+}];
 
 beforeEach((done) => {
   Todo.remove({}).then(() => {
@@ -98,4 +98,41 @@ describe('GET /todos:id', () => {
     .expect(404)
     .end(done);
   })
+});
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    var hexId = todos[1]._id.toHexString();
+    request(app)
+    .delete(`/todos/${hexId}`)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todo._id).toBe(hexId);
+    })
+    .end((err,res) => {
+      if(err){
+        return done(err);
+      }
+      Todo.findById(hexId).then((todo) => {
+        expect(todo).toNotExist();
+        done();
+      }).catch((e) => done(e));
+    });
+  });
+
+
+  it('should return 404 if todo not found', (done) => {
+    let id = new ObjectID().toHexString();
+    request(app)
+    .delete(`/todos/${id}`)
+    .expect(404)
+    .end(done);
+  });
+
+  it('should return 404 if ObjectID is invalid', (done) => {
+    request(app)
+    .delete(`/todos/123abc`)
+    .expect(404)
+    .end(done);
+  });
 });
