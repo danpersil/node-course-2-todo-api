58b496978b7d138fb79db11cf413bd8416d27e1f
Daniel Pereira da Silva <AVA092@einstein.br>
2018-05-03 22:28:45 -0300

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const {mongoose} = require('./db/mongoose');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -46,7 +47,7 @@ app.get('/todos/:id', (req, res) => {
     if(!todo){
       return res.status(404).send();
     }
-    res.status(200).send({todo});
+    res.send({todo});
 
   }).catch(() => res.status(400).send());
 });
@@ -66,8 +67,34 @@ app.delete('/todos/:id', (req, res) => {
   });
 });
 
-app.listen(port, () => {
-  console.log(`Started on port ${port}.`);
-});
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
 
-module.exports = {app};
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  }else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findOneAndUpdate(id,
+    { $set: body }, { new: true }).then((todo) => {
+      if(!todo){
+        return res.status(404);
+      }
+      res.send({todo});
+    }).catch((e) => {
+      res.status(400).send(e);
+    });
+  });
+
+  app.listen(port, () => {
+    console.log(`Started on port ${port}.`);
+  });
+
+  module.exports = {app};
